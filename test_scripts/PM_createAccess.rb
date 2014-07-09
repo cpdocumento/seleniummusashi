f6f7ef9a1736a1dbdb959ee41a9ea7699ee18f6d
Caroline Jandayan <cjandayan@musashi.ph>
2014-07-09 11:13:18 +0800

    july 9 keypair create and delete

--- test_scripts/PM_createAccess.rb
+++ test_scripts/PM_createAccess.rb
@@ -1,14 +1,19 @@
 current_directory = File.expand_path(File.dirname(__FILE__))
 require current_directory + "/../test_helper"
+require "sqlite3"
 
-class XXXXXX < Test::Unit::TestCase
+class CreateAccess < MiniTest::Test
 
-  include Common::XXXXXX
+  include Common::AuthenticationHelper
+  include Common::UsersHelper
+  include Common::KeypairHelper
+  include Common::SecurityGroupHelper
+  include Common::FloatingIPHelper
 
   def setup
     @test_data = Data.config.test_data
     @config = Data.config.setup
-    
+    @db = ::SQLite3::Database.new "testdb.db"
     @driver = Selenium::WebDriver.for @config["test_browser"].to_sym
     @driver.get(@config["envi"] + "/")
   end
@@ -17,7 +22,12 @@ class XXXXXX < Test::Unit::TestCase
     @driver.quit
   end
   
-  def XXXXXX
-  
+  def test_import_keypair
+    login(@driver, @test_data["user_mem"], @test_data["user_password"])
+    @driver.find_element(:css, "i.fa.fa-lock").click
+    for i in 1..10
+      import_keypair(@driver, @test_data["keypair_name"] + i.to_s, @test_data["keypair_keys"])
+    end
   end
+
 end
--- test_scripts/PM_deleteAccess.rb
+++ test_scripts/PM_deleteAccess.rb
@@ -1,14 +1,19 @@
 current_directory = File.expand_path(File.dirname(__FILE__))
 require current_directory + "/../test_helper"
+require "sqlite3"
 
-class XXXXXX < Test::Unit::TestCase
+class DeleteAccess < MiniTest::Test
 
-  include Common::XXXXXX
+  include Common::AuthenticationHelper
+  include Common::UsersHelper
+  include Common::KeypairHelper
+  include Common::SecurityGroupHelper
+  include Common::FloatingIPHelper
 
   def setup
     @test_data = Data.config.test_data
     @config = Data.config.setup
-    
+    @db = ::SQLite3::Database.new "testdb.db"
     @driver = Selenium::WebDriver.for @config["test_browser"].to_sym
     @driver.get(@config["envi"] + "/")
   end
@@ -17,7 +22,17 @@ class XXXXXX < Test::Unit::TestCase
     @driver.quit
   end
   
-  def XXXXXX
-  
+  def test_delete_keypair
+    login(@driver, @test_data["user_mem"], @test_data["user_password"])
+    @driver.find_element(:css, "i.fa.fa-lock").click
+    !60.times{ break if (@driver.find_element(:xpath, "//div[@id='dash-access']/table[3]/tbody/tr[2]/td").displayed? rescue false); sleep 1 }
+    for i in 1..10
+      row =2
+      !60.times{ break if (@driver.find_element(:xpath, "//div[@id='dash-access']/table[3]/tbody/tr[#{row}]/td").displayed? rescue false); sleep 1 }
+      keypair_name = @driver.find_element(:xpath, "//div[@id='dash-access']/table[3]/tbody/tr[#{row}]/td").text
+      delete_keypair(@driver, keypair_name, row)
+      row +=1
+    end
   end
+	 
 end
