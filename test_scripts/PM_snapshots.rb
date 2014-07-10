b3c8d1923975592b86834ec86a5ce1aa3c8e1b8a
Kimberley V. Fajardo <kfajardo@morphlabs.com>
2014-07-10 14:05:13 -0700

    PM creates snapshots

--- test_scripts/PM_snapshots.rb
+++ test_scripts/PM_snapshots.rb
@@ -1,14 +1,15 @@
 current_directory = File.expand_path(File.dirname(__FILE__))
 require current_directory + "/../test_helper"
 
-class XXXXXX < Test::Unit::TestCase
+class CreateSnapshot < Minitest::Test
 
-  include Common::XXXXXX
+  include Common::InstanceHelper
+  include Common::AuthenticationHelper
 
   def setup
     @test_data = Data.config.test_data
     @config = Data.config.setup
-    
+    @db = SQLite3::Database.new "testdb.db"
     @driver = Selenium::WebDriver.for @config["test_browser"].to_sym
     @driver.get(@config["envi"] + "/")
   end
@@ -17,7 +18,20 @@ class XXXXXX < Test::Unit::TestCase
     @driver.quit
   end
   
-  def XXXXXX
-  
+  def test_createSnapshot
+    @driver.manage().window().maximize()
+    wait = Selenium::WebDriver::Wait.new(:timeout => 20)
+    result = @db.execute("select pm from userindex").first
+    current_pm_index = result[0]
+
+    login(@driver, @test_data["user_mem"] + current_pm_index.to_s, @test_data["user_password"])
+    wait.until { @driver.find_element(:xpath, "//*[@id=\"head-project-name\"]/span/span").text == @test_data["user_project"] + 0.to_s }
+    
+    for i in 1..10
+      createSnapshot(@driver, @test_data["res_instance"] + i.to_s,  @test_data["res_snapshot"] + i.to_s)
+    end
+    
+    logout(@driver)
+    
   end
 end
