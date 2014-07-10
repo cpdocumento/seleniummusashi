27d2c790f9e39244469172c371338fa961de0c53
Caroline Jandayan <cjandayan@musashi.ph>
2014-07-10 09:14:54 +0800

    July 10 PM_deleteAccess.rb

--- test_scripts/PM_deleteAccess.rb
+++ test_scripts/PM_deleteAccess.rb
@@ -21,7 +21,13 @@ class DeleteAccess < MiniTest::Test
   end
   
   def test_delete_keypair
-    login(@driver, @test_data["user_mem"], @test_data["user_password"])
+    @driver.manage().window().maximize()
+    wait = Selenium::WebDriver::Wait.new(:timeout => 20)
+    result = @db.execute("select pm from userindex").first
+    current_pm_index = result[0]
+		
+    login(@driver, @test_data["user_mem"] + current_pm_index.to_s, @test_data["user_password"])
+    wait.until { @driver.find_element(:xpath, "//*[@id=\"head-project-name\"]/span/span").text == @test_data["user_project"] + 0.to_s }
     for i in 1..10
       delete_keypair(@driver, @test_data["res_keypair"] + i.to_s)
     end
