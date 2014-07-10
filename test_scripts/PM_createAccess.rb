783c3df59a9b32f9108d88604579a9d9266a05d5
Caroline Jandayan <cjandayan@musashi.ph>
2014-07-10 13:58:08 +0800

    July 10 Security Group create and delete

--- test_scripts/PM_createAccess.rb
+++ test_scripts/PM_createAccess.rb
@@ -7,6 +7,7 @@ class CreateAccess < MiniTest::Test
   include Common::AuthenticationHelper
   include Common::UsersHelper
   include Common::KeypairHelper
+  include Common::SecurityGroupHelper
 
   def setup
     @test_data = Data.config.test_data
@@ -30,6 +31,19 @@ class CreateAccess < MiniTest::Test
     wait.until { @driver.find_element(:xpath, "//*[@id=\"head-project-name\"]/span/span").text == @test_data["user_project"] + 0.to_s }
     for i in 1..10
       import_keypair(@driver, @test_data["res_keypair"] + i.to_s, @test_data["keypair_keys"])
+    end
+  end
+	
+  def test_create_secgroup
+    @driver.manage().window().maximize()
+    wait = Selenium::WebDriver::Wait.new(:timeout => 20)
+    result = @db.execute("select pm from userindex").first
+    current_pm_index = result[0]
+		
+    login(@driver, @test_data["user_mem"] + current_pm_index.to_s, @test_data["user_password"])
+    wait.until { @driver.find_element(:xpath, "//*[@id=\"head-project-name\"]/span/span").text == @test_data["user_project"] + 0.to_s }
+    for i in 1..10
+      create_secgroup(@driver, @test_data["res_secgroup"] + i.to_s, @test_data["common_description"])
     end
   end
 
