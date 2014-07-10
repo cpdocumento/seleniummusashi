2131dd75f17df44668bb7872dadec5a03ffd6cb3
Kimberley V. Fajardo <kfajardo@morphlabs.com>
2014-07-09 23:59:09 -0700

    PM detaches IPs
    Updates to detaching an IP
    
    Updates to detaching an IP2

--- helpers/floatingip_helper.rb
+++ helpers/floatingip_helper.rb
@@ -21,22 +21,23 @@ module Common
     wait.until { (driver.find_element(:xpath, "//*[@id=\"dash-access\"]/table[1]/tbody/tr/td[normalize-space(text())=\"#{ ip }\"]/..//td[3]").text == instance_name) rescue false; sleep 1 }
   end
 
-  def detachIP(driver, ip)
+  def detachIP(driver, instance_name)
     wait = Selenium::WebDriver::Wait.new(:timeout => 180)
     
     wait.until { driver.find_element(:css, "i.fa.fa-lock").displayed? }
     driver.find_element(:css, "i.fa.fa-lock").click
 
-    # click attach button of ip
-    sleep 2
-    driver.find_element(:xpath, "//*[@id=\"dash-access\"]/table[1]/tbody/tr/td[normalize-space(text())=\"#{ ip }\"]/..//td/div/button[1]").click
+    # click detach button of ip that is attached to instance
+    wait.until { @driver.find_element(:xpath, "//*[@id=\"dash-access\"]/table[1]/tbody/tr[2]/td[2]").displayed? } 
+    ip = driver.find_element(:xpath, "//*[@id=\"dash-access\"]/table[1]/tbody/tr/td[normalize-space(text())=\"#{ instance_name }\"]/..//td[2]").text
+    driver.find_element(:xpath, "//*[@id=\"dash-access\"]/table[1]/tbody/tr/td[normalize-space(text())=\"#{ instance_name }\"]/..//td/div/button[1]").click
     
     # confirmation message
     wait.until { driver.find_element(:xpath, "//*[@id=\"dash-access\"]/div[2]").displayed? }
     driver.find_element(:xpath, "//*[@id=\"dash-access\"]/div[2]/div/button[1]").click
     
     # wait until the IP is detached
-    wait.until { (driver.find_element(:xpath, "//*[@id=\"dash-access\"]/table[1]/tbody/tr/td[normalize-space(text())=\"#{ ip }\"]/..//td/div/button[1]").text == /Attach/) rescue false; sleep 1 }
+    wait.until { (driver.find_element(:xpath, "//*[@id=\"dash-access\"]/table[1]/tbody/tr/td[normalize-space(text())=\"#{ ip }\"]/..//td/div/button[1]").text =~ /Attach/) rescue false; sleep 1 }
   end
 
   end
