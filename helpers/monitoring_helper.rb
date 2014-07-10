725e3b7eb2cbfa204cf45b5bd76157967225760c
ceilomar documento <ceilo.doc143@gmail.com>
2014-07-11 01:04:35 +0800

    update pm monitoring

--- helpers/monitoring_helper.rb
+++ helpers/monitoring_helper.rb
@@ -47,5 +47,47 @@ module Common
 		!60.times{ break if (driver.find_element(:xpath, "//*[@id='monitoring-settings']/div[2]/div/div/p/p")).text == "Monitoring settings has been successfully updated." rescue false; sleep 2}
 	end
 
+	def update_instance_monitoring(driver, instance_name, warning, error)
+		wait = Selenium::WebDriver::Wait.new(:timeout => 120)
+    sleep 2
+    
+    # go to instances page    
+    wait.until { driver.find_element(:css, "i.fa.fa-hdd-o").displayed? }
+    driver.find_element(:css, "i.fa.fa-hdd-o").click
+    
+    # go to instance details page
+    sleep 2
+    assert !60.times{ break if (driver.find_element(:xpath, "//tr/td[@id=\"instance-name\"]/a[normalize-space(text())=\"#{ instance_name }\"]").displayed? ) rescue false; sleep 2 }
+    wait.until { driver.find_element(:link, instance_name).displayed? }
+    driver.find_element(:link, instance_name).click
+    wait.until { driver.find_element(:xpath, "//ul[@ng-show=\"instance.actions\"]").displayed? }
+    driver.find_element(:xpath, "//*[@id='details-action']/ul[2]/li[9]/a").click
+
+    wait.until { driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[3]/div[2]/h4/span").displayed? }
+
+    #cpu
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[4]/div[1]/div[3]/div[1]/form/div[1]/div[2]/div[1]/input").clear
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[4]/div[1]/div[3]/div[1]/form/div[1]/div[2]/div[1]/input").send_keys(warning)
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[4]/div[1]/div[3]/div[1]/form/div[2]/div[2]/div[1]/input").clear
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[4]/div[1]/div[3]/div[1]/form/div[2]/div[2]/div[1]/input").send_keys(error)
+
+		#memory
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[4]/div[2]/div[3]/div[1]/form/div[1]/div[2]/div[1]/input").clear
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[4]/div[2]/div[3]/div[1]/form/div[1]/div[2]/div[1]/input").send_keys(warning)
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[4]/div[2]/div[3]/div[1]/form/div[2]/div[2]/div[1]/input").clear
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[4]/div[2]/div[3]/div[1]/form/div[2]/div[2]/div[1]/input").send_keys(error)
+
+		#storage
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[4]/div[3]/div[3]/div[1]/form/div[1]/div[2]/div[1]/input").clear
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[4]/div[3]/div[3]/div[1]/form/div[1]/div[2]/div[1]/input").send_keys(warning)
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[4]/div[3]/div[3]/div[1]/form/div[2]/div[2]/div[1]/input").clear
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[4]/div[3]/div[3]/div[1]/form/div[2]/div[2]/div[1]/input").send_keys(error)
+
+		#save settings
+		driver.find_element(:xpath, "//*[@id='instance-monitoring-settings']/div[7]/button[1]").click
+
+		#back to instances page
+		driver.find_element(:css, "i.fa.fa-hdd-o").click
   end
+end
 end
--- test_scripts/PM_monitoring.rb
+++ test_scripts/PM_monitoring.rb
@@ -1,9 +1,10 @@
 current_directory = File.expand_path(File.dirname(__FILE__))
 require current_directory + "/../test_helper"
 
-class XXXXXX < Test::Unit::TestCase
+class PMonitoring < MiniTest::Test
 
-  include Common::XXXXXX
+  include Common::AuthenticationHelper
+  include Common::MonitoringHelper
 
   def setup
     @test_data = Data.config.test_data
@@ -17,7 +18,22 @@ class XXXXXX < Test::Unit::TestCase
     @driver.quit
   end
   
-  def XXXXXX
-  
+  def test_pm_monitoring
+    login(@driver, @test_data["user_mem"] + 1.to_s, @test_data["user_password"])
+
+    warning = 30
+    error = 35
+    increase = 5
+    range = 10 
+
+    range.times do 
+
+      warning += increase
+      error += increase
+
+      update_instance_monitoring(@driver, @test_data["res_instance"] + 1.to_s, warning, error)
+    end
+    
+    logout(@driver)
   end
 end
