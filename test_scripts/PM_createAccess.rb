670fbe67d0a9480a99367659d67047a2ab5a9829
Kimberley V. Fajardo <kfajardo@morphlabs.com>
2014-07-11 00:06:54 -0700

    Updates to secgrouphelper

--- test_scripts/PM_createAccess.rb
+++ test_scripts/PM_createAccess.rb
@@ -21,31 +21,31 @@ class CreateAccess < MiniTest::Test
     @driver.quit
   end
   
-  def test_import_keypair
-    @driver.manage().window().maximize()
-    wait = Selenium::WebDriver::Wait.new(:timeout => 20)
-    result = @db.execute("select pm from userindex").first
-    current_pm_index = result[0]
-		
-    login(@driver, @test_data["user_mem"] + current_pm_index.to_s, @test_data["user_password"])
-    wait.until { @driver.find_element(:xpath, "//*[@id=\"head-project-name\"]/span/span").text == @test_data["user_project"] + 0.to_s }
-    for i in 1..10
-      import_keypair(@driver, @test_data["res_keypair"] + i.to_s, @test_data["keypair_keys"])
-    end
-  end
-	
-  def test_create_secgroup
-    @driver.manage().window().maximize()
-    wait = Selenium::WebDriver::Wait.new(:timeout => 20)
-    result = @db.execute("select pm from userindex").first
-    current_pm_index = result[0]
-		
-    login(@driver, @test_data["user_mem"] + current_pm_index.to_s, @test_data["user_password"])
-    wait.until { @driver.find_element(:xpath, "//*[@id=\"head-project-name\"]/span/span").text == @test_data["user_project"] + 0.to_s }
-    for i in 1..10
-      create_secgroup(@driver, @test_data["res_secgroup"] + i.to_s, @test_data["common_description"])
-    end
-  end
+  #def test_import_keypair
+  #  @driver.manage().window().maximize()
+  #  wait = Selenium::WebDriver::Wait.new(:timeout => 20)
+  #  result = @db.execute("select pm from userindex").first
+  #  current_pm_index = result[0]
+  #
+  #  login(@driver, @test_data["user_mem"] + current_pm_index.to_s, @test_data["user_password"])
+  #  wait.until { @driver.find_element(:xpath, "//*[@id=\"head-project-name\"]/span/span").text == @test_data["user_project"] + 0.to_s }
+  #  for i in 1..10
+  #    import_keypair(@driver, @test_data["res_keypair"] + i.to_s, @test_data["keypair_keys"])
+  #  end
+  #end
+  #
+  #def test_create_secgroup
+  #  @driver.manage().window().maximize()
+  #  wait = Selenium::WebDriver::Wait.new(:timeout => 20)
+  #  result = @db.execute("select pm from userindex").first
+  #  current_pm_index = result[0]
+  #
+  #  login(@driver, @test_data["user_mem"] + current_pm_index.to_s, @test_data["user_password"])
+  #  wait.until { @driver.find_element(:xpath, "//*[@id=\"head-project-name\"]/span/span").text == @test_data["user_project"] + 0.to_s }
+  #  for i in 1..10
+  #    create_secgroup(@driver, @test_data["res_secgroup"] + i.to_s, @test_data["common_description"])
+  #  end
+  #end
 
   def test_add_rule
     login(@driver, @test_data["user_mem"] + 1.to_s, @test_data["user_password"])
